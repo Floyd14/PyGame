e79260f02818157e061ce9b43544c825d9732b34
Floyd14 <andreafloyd14@icloud.com>
2015-11-27 17:19:33 +0100

    hhhhhhh

--- PyGame/PyGameProg/PyGameBook/5_MyTest.py
+++ PyGame/PyGameProg/PyGameBook/5_MyTest.py
@@ -1,45 +1,35 @@
 # -*- coding: utf_8 -*-
 
 '''
-This is a game test for first 4th lessons'''
+This is a game test for first 4th lessons. 
+
+The program should:
+- loads a backround, and an image that moves foward and back
+- '''
 
 import pygame as pg
 import os
 
-###
+pg.init()
+pg.display.set_mode((640,480), )
+pg.display.set_caption('A Title...')
 
-opts = {'width': 800,
-        'height': 600,
-        'backcol': (0, 0, 0),
-        'fps': 100}
-
-###
-
-def setScreen(width=800, height=600, collision=True, **opts):
-    screen_surface = pg.display.set_mode( (width,height), )
-    if collision:
-        screen_rect = screen_surface.get_rect()
-
-
+surface = pg.Surface(screen.get_size())
+surface.convert()
+backgroung_image = pg.image.load('./data/ente.jpg', )
 
 mainloop = True
-while mainloop:  
-    pg.init()
-    setScreen()
-    clock = pg.time.Clock()
-    milliseconds = clock.tick( opts['fps'] )      # do not go faster than this framerate
-    playtime += milliseconds / 1000.0
-    
-    
-    
-    # for stopping the mainloop
+while mainloop:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             mainloop = False # pygame window closed by user
         elif event.type == pg.KEYDOWN:
             if event.key == pg.K_ESCAPE:
                 mainloop = False # user pressed ESC
-                
-               
-
-
+    
+    surface.blit(backgroung_image,(0,0)
+    
+    
+    
+    
+    
