5cafa05fe3f17ada0f5694a255834ae6eb36d104
Floyd14 <andreafloyd14@icloud.com>
2015-11-30 01:54:23 +0100

    New

--- PyGame/PyGameProg/PyGameBook/Ex1.py
+++ PyGame/PyGameProg/PyGameBook/Ex1.py
@@ -20,8 +20,6 @@ import pygame
 ####
 
 class PygView(object):
-    
-
     def __init__(self, width=640, height=400, fps=60):
         """Initialize pygame, window, background, font,...
         """
@@ -29,14 +27,14 @@ class PygView(object):
         pygame.display.set_caption("Press ESC to quit")
         self.width = width
         self.height = height
-        #self.height = width // 4
+        # self.height = width // 4
         self.screen = pygame.display.set_mode((self.width, self.height), pygame.DOUBLEBUF)
         self.background = pygame.Surface(self.screen.get_size()).convert()
         self.clock = pygame.time.Clock()
         self.fps = fps
         self.playtime = 0.0
-        self.font = pygame.font.SysFont('mono', 20, bold=True)
-
+        # self.font = pygame.font.SysFont("mono", 20, bold=True)
+        self.font = pygame.font.get_default_font()
 
     def run(self):
         """The mainloop
@@ -53,25 +51,24 @@ class PygView(object):
             milliseconds = self.clock.tick(self.fps)
             self.playtime += milliseconds / 1000.0
             self.draw_text("FPS: {:6.3}{}PLAYTIME: {:6.3} SECONDS".format(
-                           self.clock.get_fps(), " "*5, self.playtime))
+                self.clock.get_fps(), " " * 5, self.playtime))
 
             pygame.display.flip()
             self.screen.blit(self.background, (0, 0))
 
         pygame.quit()
 
-
     def draw_text(self, text):
         """Center text in window
         """
-        fw, fh = self.font.size(text) # fw: font width,  fh: font height
+        fw, fh = self.font.size(text)  # fw: font width,  fh: font height
         surface = self.font.render(text, True, (0, 255, 0))
         # // makes integer division in python3
         self.screen.blit(surface, ((self.width - fw) // 2, (self.height - fh) // 2))
 
+
 ####
 
 if __name__ == '__main__':
-
     # call with width of window and fps
     PygView(640, 400).run()
